1c18d4b8e485b0b5a6c140b9b976e3aae62c9735
javis <huang.javis@gmail.com>
2023-09-03 22:21:23 +0800

    引用jquery 3.6.4、bootstrap 5.0.2、angularjs1.8.3 新增web3.0帳戶查詢工具

---
+++ web3.0_account_look/tokens.js
@@ -0,0 +1,23 @@
+var tokens = {};
+tokens[1]={};
+tokens[1].Main = {name:"ETH"};
+tokens[1].CoinID = "ethereum";
+tokens[1].subTokens = [];
+tokens[1].subTokens.push('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'); //USDC
+tokens[1].subTokens.push('0xdAC17F958D2ee523a2206206994597C13D831ec7'); //USDT
+tokens[1].subTokens.push('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'); //WETH
+
+tokens[56]={};
+tokens[56].Main = {name:"BNB"};
+tokens[56].CoinID = "binancecoin";
+tokens[56].subTokens = [];
+tokens[56].subTokens.push('0x55d398326f99059ff775485246999027b3197955'); //USDT
+tokens[56].subTokens.push('0x3fda9383a84c05ec8f7630fe10adf1fac13241cc'); //DEGO
+tokens[56].subTokens.push('0x42712df5009c20fee340b245b510c0395896cf6e'); //DFT
+tokens[56].subTokens.push('0x373233A38ae21cF0C4f9DE11570E7D5Aa6824A1E'); //ALPACA
+tokens[56].subTokens.push('0x04645027122c9f152011f128c7085449b27cb6d7'); //ARKR.org
+tokens[56].subTokens.push('0x740b40760266d54362c222c0490a95cf970cd199'); //JCL
+tokens[56].subTokens.push('0x7c357cd85bb600f748ec1d82adef74464c13e7a7'); //GPI
+tokens[56].subTokens.push('0xf0d585a29a86c25819526ba494100951dc31aa0b'); //NOR
+tokens[56].subTokens.push('0x366945ba85881b77c186597b4639683efeeb65ca'); //CZBI
+tokens[56].subTokens.push('0xfb9f5738c9d767fea5af6e4d826ce18d1a48589a'); //DAD
