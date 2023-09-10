2f2defc175082606cd88881cabcb4324b738a25d
javis <huang.javis@gmail.com>
2023-09-10 22:43:02 +0800

    web3.0帳戶查詢工具： 增加BASE鏈跟Arbitrum Nova鏈 增加一些幣

--- web3.0_account_look/tokens.js
+++ web3.0_account_look/tokens.js
@@ -38,6 +38,18 @@ tokens[56].subTokens.push('0x7c357cd85bb600f748ec1d82adef74464c13e7a7'); //GPI
 tokens[56].subTokens.push('0xf0d585a29a86c25819526ba494100951dc31aa0b'); //NOR
 tokens[56].subTokens.push('0x366945ba85881b77c186597b4639683efeeb65ca'); //CZBI
 tokens[56].subTokens.push('0xfb9f5738c9d767fea5af6e4d826ce18d1a48589a'); //DAD
+tokens[56].subTokens.push('0xfecCa80fF6DeB2B492E93df3B67f0C523Cfd3a48'); //ALGOBLK
+tokens[56].subTokens.push('0x07dA81e9a684AB87FAd7206b3bC8d0866f48cC7C'); //BEAN
+tokens[56].subTokens.push('0xFE8bF5B8F5e4eb5f9BC2be16303f7dAB8CF56aA8'); //BIBI
+tokens[56].subTokens.push('0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'); //BUSD
+tokens[56].subTokens.push('0x3019BF2a2eF8040C242C9a4c5c4BD4C81678b2A1'); //GMT
+tokens[56].subTokens.push('0x2dfF88A56767223A5529eA5960Da7A3F5f766406'); //ID
+tokens[56].subTokens.push('0x9eeB6c5ff183E6001c65a12D70026b900AE76781'); //IRENA
+tokens[56].subTokens.push('0x843D4a358471547f51534e3e51fae91cb4Dc3F28'); //LOWB
+tokens[56].subTokens.push('0x9AeB2E6DD8d55E14292ACFCFC4077e33106e4144'); //PAYU
+tokens[56].subTokens.push('0x7565ab68D3F9DaDff127F864103C8c706Cf28235'); //TFI
+tokens[56].subTokens.push('0x29A5DaF6E3FDF964DEf07706eA1ABee7EC03D021'); //TLC
+tokens[56].subTokens.push('0xa75d9ca2a0a1D547409D82e1B06618EC284A2CeD'); //WMX
 
 tokens[137]={};
 tokens[137].Network = "https://polygon.meowrpc.com";
@@ -51,11 +63,25 @@ tokens[137].subTokens.push('0xBAC3368B5110f3a3ddA8B5a0F7B66EDb37C47AFe'); //AIPE
 tokens[137].subTokens.push('0x4193103fbd8eBdD4F50aCc37E024037E0a2392f9'); //AIWORDS
 
 tokens[324]={};
-tokens[324].Network = "https://zksync.meowrpc.com";
+tokens[324].Network = "https://mainnet.era.zksync.io";
 tokens[324].Main = "zksync Era";
 tokens[324].Coin = "ETH";
 tokens[324].CoinID = "ethereum";
 tokens[324].subTokens = [];
+/*
+tokens[324].subTokens.push('0xd599dA85F8Fc4877e61f547dFAcffe1238A7149E'); //Cheems
+tokens[324].subTokens.push('0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4'); //USDC
+tokens[324].subTokens.push('0xe62b571E9F40D158cB20796C56E93475d896c56D'); //xUSDC
+tokens[324].subTokens.push('0x47EF4A5641992A72CFd57b9406c9D9cefEE8e0C4'); //ZAT
+ */
+
+tokens[8453]={};
+tokens[8453].Network = "https://developer-access-mainnet.base.org";
+tokens[8453].Main = "BASE";
+tokens[8453].Coin = "ETH";
+tokens[8453].CoinID = "ethereum";
+tokens[8453].subTokens = [];
+tokens[8453].subTokens.push('0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA'); //USDBC
 
 tokens[42161]={};
 tokens[42161].Network = "https://arbitrum.meowrpc.com";
@@ -67,3 +93,18 @@ tokens[42161].subTokens.push('0xFF191514A9baba76BfD19e3943a4d37E8ec9a111'); //AG
 tokens[42161].subTokens.push('0x09E18590E8f76b6Cf471b3cd75fE1A1a9D2B2c2b'); //AIDOGE
 tokens[42161].subTokens.push('0x912CE59144191C1204E64559FE8253a0e49E6548'); //ARB
 tokens[42161].subTokens.push('0xF1A82bfA7fCEb8B8741e7E04a6B8EfD348cA6393'); //AISHIB
+tokens[42161].subTokens.push('0x11524A5765066A5362Ac184f7dB9FF51286Fe99a'); //AIPANDA
+tokens[42161].subTokens.push('0x53bCF6698C911b2A7409a740EACDDB901fC2a2C6'); //KABOSU
+tokens[42161].subTokens.push('0x9c8eF67C9168587134e1F29a71405DC753500A45'); //LG
+tokens[42161].subTokens.push('0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'); //USDCE
+tokens[42161].subTokens.push('0x6fD58f5a2F3468e35fEb098b5F59F04157002407'); //POGAI
+tokens[42161].subTokens.push('0x1D987200dF3B744CFa9C14f713F5334CB4Bc4D5D'); //REKT
+
+tokens[42170]={};
+tokens[42170].Network = "https://arbitrum-nova.publicnode.com";
+tokens[42170].Main = "Arbitrum Nova";
+tokens[42170].Coin = "ETH";
+tokens[42170].CoinID = "ethereum";
+tokens[42170].subTokens = [];
+tokens[42170].subTokens.push('0xD88492b380bc26593aE032ABF12ea9B135Ed6534'); //nMKX
+tokens[42170].subTokens.push('0xAbDda01e54B894c760f69778a760624cCb159e01'); //XTM
